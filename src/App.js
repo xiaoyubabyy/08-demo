35ecfe8875ffed676cd4647ee8ca66ac9374eb59
Xiaoyu <554751389@qq.com>
2016-11-29 16:27:50 +0800

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import NavHeader from './component/NavHeader'
+import Footer from './component/footer'
 class App extends React.Component {
   render () {
     return(
-      <div>
-
+      <div className='my-wrap'>
+        <NavHeader />
+        <div className='main'>
+        {this.props.children}
+        </div>
+        <Footer />
       </div>
     )
   }
